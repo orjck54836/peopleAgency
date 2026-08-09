79833cc6fcfe74ef515b56ed53fd14e71ed651ea
orjck54836 <69657178+orjck54836@users.noreply.github.com>
2026-08-09 18:04:32 +0800

    0808改版

---
+++ plugins/fbpixel.client.ts
@@ -0,0 +1,23 @@
+export default defineNuxtPlugin(() => {
+  const pixelId = '1568271991459706'
+
+  const script = document.createElement('script')
+  script.innerHTML = `
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window,document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '${pixelId}');
+    fbq('track', 'PageView');
+  `
+  document.head.appendChild(script)
+
+  // noscript fallback
+  const noscript = document.createElement('noscript')
+  noscript.innerHTML = `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1"/>`
+  document.body.appendChild(noscript)
+})
